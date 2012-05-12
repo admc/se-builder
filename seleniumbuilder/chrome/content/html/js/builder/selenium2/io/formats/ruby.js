0aeff2ba3aabfba56c0758985a4f5a0b7a33d69e
msabourin <mathieu.c.sabourin@gmail.com>
2012-05-11 18:04:32 -0700

    Started work on ruby mode, got up to verifyPageSource from the python mode.

---
+++ seleniumbuilder/chrome/content/html/js/builder/selenium2/io/formats/ruby.js
@@ -0,0 +1,170 @@
+builder.selenium2.io.formats.push(builder.selenium2.io.createLangFormatter({
+  name: "Ruby",
+  extension: ".rb",
+  not: "not ",
+  start:
+    "require 'rubygems'\n" +
+	"require 'selenium-webdriver'"+
+	"\n" +
+	"wd = Selenium::WebDriver.for :firefox\n" +
+  end:
+    "wd.quit\n",
+  lineForType: {
+    "get":
+      "wd.get \"{url}\"\n",
+    "goBack":
+      "wd.navigate.back\n",
+    "goForward":
+      "wd.navigate.forward\n",
+    "clickElement":
+      "wd.find_element({locatorBy}, \"{locator}\").click\n",
+    "sendKeysToElement":
+      "wd.find_element({locatorBy}, \"{locator}\").send_keys \"{text}\"\n",
+    "setElementSelected":
+      "if not wd.find_element({locatorBy}, \"{locator}\").selected?\n" +
+	  "  wd.{locatorBy}(\"{locator}\").click\n"+
+	  "end\n",
+    "setElementNotSelected":
+      "if wd.find_element({locatorBy}, \"{locator}\").selected?\n" +
+	  "  wd.{locatorBy}(\"{locator}\").click\n"+
+	  "end\n",
+    "submitElement":
+      "wd.find_element({locatorBy}, \"{locator}\").submit\n",
+    "close":
+      "",
+    "verifyTextPresent":
+      "if wd.find_element(:tag_name, \"html\").text.include? {posNot}\"{text}\"\n" +
+	  "  print \"{negNot}verifyTextPresent failed\"\n"+
+	  "end\n",
+    "assertTextPresent":
+      "if wd.find_element(:tag_name, \"html\").text.include? {posNot}\"{text}\"\n" +
+	  "  wd.quit\n" +
+	  "  raise Exception, \"{negNot}assertTextPresent failed\"\n"+
+	  "end\n",
+    "waitForTextPresent":
+      "",
+    "verifyBodyText":
+      "if {posNot}\"{text}\" == wd.find_element(:tag_name, \"html\").text\n" +
+	  "  print \"{negNot}verifyBodyText failed\" \n"+
+	  "end\n",
+    "assertBodyText":
+      "if {posNot}\"{text}\" == wd.find_element_by_tag_name(\"html\").text\n" +
+	  "  wd.close()\n" +
+	  "  raise Exception, \"{negNot}assertBodyText failed\"\n"+
+	  "end\n",
+    "waitForBodyText":
+      "",
+    "verifyElementPresent":
+      "if {negNot}wd.find_element({locatorBy}, \"{locator}\").length == 0\n" +
+	  "  print(\"{negNot}verifyElementPresent failed\")\n"+
+	  "end",
+    "assertElementPresent":
+      "if {negNot}wd.find_element({locatorBy}, \"{locator}\").length == 0\n" +
+	  "  wd.quit\n" +
+	  "  raise Exception, \"{negNot}assertElementPresent failed\"\n"+
+	  "end\n",
+    "waitForElementPresent":
+      "",
+    "verifyPageSource":
+      "if {posNot}(wd.get_page_source() == \"{source}\")\n" +
+	  "  print \"{negNot}verifyPageSource failed\"\n"+
+	  "end\n",
+    "assertPageSource":
+      "if {posNot}wd.get_page_source() == \"{source}\":\n" +
+      "    wd.close()\n" +
+      "    raise Exception(\"{negNot}assertPageSource failed\")\n",
+    "waitForPageSource":
+      "",
+    "verifyText":
+      "if {posNot}wd.{locatorBy}(\"{locator}\").text == \"{text}\":\n" +
+      "    print(\"{negNot}verifyText failed\")\n",
+    "assertText":
+      "if {posNot}wd.{locatorBy}(\"{locator}\").text == \"{text}\":\n" +
+      "    wd.close()\n" +
+      "    raise Exception(\"{negNot}assertText failed\")\n",
+    "waitForText":
+      "",
+    "verifyCurrentUrl":
+      "if {posNot}wd.current_url == \"{url}\":\n" +
+      "    print(\"{negNot}verifyCurrentUrl failed\")\n",
+    "assertCurrentUrl":
+      "if {posNot}wd.current_url == \"{url}\":\n" +
+      "    wd.close()\n" +
+      "    raise Exception(\"{negNot}assertCurrentUrl failed\")\n",
+    "waitForCurrentUrl":
+      "",
+    "verifyTitle":
+      "if {posNot}wd.title == \"{title}\":\n" +
+      "    print(\"{negNot}verifyTitle failed\")\n",
+    "assertTitle":
+      "if {posNot}wd.title == \"{title}\":\n" +
+      "    wd.close()\n" +
+      "    raise Exception(\"{negNot}assertTitle failed\")\n",
+    "waitForTitle":
+      "",
+    "verifyElementSelected":
+      "if {posNot}wd.{locatorBy}(\"{locator}\").is_selected():\n" +
+      "    print(\"{negNot}verifyElementSelected failed\")\n",
+    "assertElementSelected":
+      "if {posNot}wd.{locatorBy}(\"{locator}\").is_selected():\n" +
+      "    wd.close()\n" +
+      "    raise Exception(\"{negNot}assertElementSelected failed\")\n",
+    "waitForElementSelected":
+      "",
+    "verifyElementValue":
+      "if {posNot}wd.{locatorBy}(\"{locator}\").value == \"{value}\":\n" +
+      "    print(\"{negNot}verifyElementValue failed\")\n",
+    "assertElementValue":
+      "if {posNot}wd.{locatorBy}(\"{locator}\").value == \"{value}\":\n" +
+      "    wd.close()\n" +
+      "    raise Exception(\"{negNot}assertElementValue failed\")\n",
+    "element.waitForValue":
+      "",
+    "verifyCookieByName":
+      "if {posNot}wd.get_cookie(\"{name}\") == \"{value}\":\n" +
+      "    print(\"{negNot}verifyCookieByName failed\")\n",
+    "assertCookieByName":
+      "if {posNot}wd.get_cookie(\"{name}\") == \"{value}\":\n" +
+      "    wd.close()\n" +
+      "    raise Exception(\"{negNot}assertCookieByName failed\")\n",
+    "waitForCookieByName":
+      "",
+    "verifyCookiePresent":
+      "if {posNot}wd.get_cookie(\"{name}\"):\n" +
+      "    print(\"{negNot}verifyCookiePresent failed\")\n",
+    "assertCookiePresent":
+      "if {posNot}wd.get_cookie(\"{name}\"):\n" +
+      "    wd.close()\n" +
+      "    raise Exception(\"{negNot}assertCookiePresent failed\")\n",
+    "waitForCookiePresent":
+      "",
+    "storeTitle":
+      "${{variable}} = wd.title\n"
+  },
+  locatorByForType: function(stepType, locatorType, locatorIndex) {
+    if ({
+      "assertElementPresent": 1,
+      "verifyElementPresent": 1
+    }[stepType.name]) {
+      return {
+        "class": "find_elements_by_class_name",
+        "id": "find_elements_by_id",
+        "link text": "find_elements_by_link_text",
+        "xpath": "find_elements_by_xpath",
+        "css selector": "find_elements_by_css_selector",
+        "name": "find_elements_by_name"}[locatorType];
+    }
+    return {
+      "class": "find_element_by_class_name",
+      "id": "find_element_by_id",
+      "link text": "find_element_by_link_text",
+      "xpath": "find_element_by_xpath",
+      "css selector": "find_element_by_css_selector",
+      "name": "find_element_by_name"}[locatorType];
+  },
+  escapeValue: function(stepType, value, pName) {
+    return value.replace(/\\/g, "\\\\").replace(/"/g, "\\\"");
+  },
+  usedVar: function(varName) { return varName; },
+  unusedVar: function(varName) { return varName; }
+}));
