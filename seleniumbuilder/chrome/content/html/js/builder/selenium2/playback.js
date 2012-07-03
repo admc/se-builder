25e4e1efb7462430c9a23c65e92defad6d1460c0
David Stark <david.stark@zarkonnen.com>
2012-07-03 13:43:15 +0200

    Fixed setElementText/sendKeysToElement playback bug.

--- seleniumbuilder/chrome/content/html/js/builder/selenium2/playback.js
+++ seleniumbuilder/chrome/content/html/js/builder/selenium2/playback.js
@@ -282,15 +282,24 @@ builder.selenium2.playback.playbackFunctions = {
   },
   "setElementText": function() {
     builder.selenium2.playback.findElement(builder.selenium2.playback.param("locator"), function(result) {
-      builder.selenium2.playback.execute('clickElement', {id: result.value.ELEMENT});
-      builder.selenium2.playback.execute('clearElement', {id: result.value.ELEMENT});
-      builder.selenium2.playback.execute('sendKeysToElement', {id: result.value.ELEMENT, value: builder.selenium2.playback.param("text").split("")});
+      builder.selenium2.playback.execute('clickElement', {id: result.value.ELEMENT},
+        function() {
+          builder.selenium2.playback.execute('clearElement', {id: result.value.ELEMENT},
+            function() {
+              builder.selenium2.playback.execute('sendKeysToElement', {id: result.value.ELEMENT, value: builder.selenium2.playback.param("text").split("")});
+            }
+          );
+        }
+      );
     });
   },
   "sendKeysToElement": function() {
     builder.selenium2.playback.findElement(builder.selenium2.playback.param("locator"), function(result) {
-      builder.selenium2.playback.execute('clickElement', {id: result.value.ELEMENT});
-      builder.selenium2.playback.execute('sendKeysToElement', {id: result.value.ELEMENT, value: builder.selenium2.playback.param("text").split("")});
+      builder.selenium2.playback.execute('clickElement', {id: result.value.ELEMENT},
+        function() {
+          builder.selenium2.playback.execute('sendKeysToElement', {id: result.value.ELEMENT, value: builder.selenium2.playback.param("text").split("")});
+        }
+      );
     });
   },
   "setElementSelected": function() {
