c18e28e4b84ec61d4fad3e243cc0cf73d2986d02
David Stark <david.stark@zarkonnen.com>
2012-07-08 16:56:16 +0200

    Reorganised remote webdriver playback for better error handling and less code duplication.

--- seleniumbuilder/chrome/content/html/js/builder/selenium2/rcPlayback.js
+++ seleniumbuilder/chrome/content/html/js/builder/selenium2/rcPlayback.js
@@ -42,7 +42,8 @@ builder.selenium2.rcPlayback.run = function(hostPort, browserstring, postRunCall
   builder.views.script.clearResults();
   jQuery('#edit-clearresults-span').show();
   builder.selenium2.rcPlayback.sessionID = null;
-  builder.selenium2.rcPlayback.post(
+  builder.selenium2.rcPlayback.send(
+    "POST",
     "",
     JSON.stringify({"desiredCapabilities":{"platform":"ANY","browserName":"firefox","version":""}}), // qqDPS
     builder.selenium2.rcPlayback.startJob);
@@ -62,16 +63,25 @@ builder.selenium2.rcPlayback.fixServerResponse = function(t) {
   return t.substring(0, i);
 };
 
-builder.selenium2.rcPlayback.startJob = function(responseText) {
-  var response = JSON.parse(responseText);
-  builder.selenium2.rcPlayback.sessionID = response.sessionId;
-  //alert(JSON.stringify(response));
-  //alert(builder.selenium2.rcPlayback.sessionID);
-  builder.selenium2.rcPlayback.playResult.success = true;
-  builder.selenium2.rcPlayback.playNextStep(null);
+builder.selenium2.rcPlayback.parseServerResponse = function(t) {
+  t = builder.selenium2.rcPlayback.fixServerResponse(t);
+  if (t.length == 0) {
+    return {};
+  } else {
+    // The response may be some JSON, or it may also be a HTML page.
+    try {
+      return JSON.parse(t);
+    } catch (e) {
+      return {};
+    }
+  }
 };
 
-
+builder.selenium2.rcPlayback.startJob = function(response) {
+  builder.selenium2.rcPlayback.sessionID = response.sessionId;
+  builder.selenium2.rcPlayback.playResult.success = true;
+  builder.selenium2.rcPlayback.playNextStep();
+};
 
 builder.selenium2.rcPlayback.playNextStep = function() {
   builder.selenium2.rcPlayback.currentStepIndex++;
@@ -79,21 +89,24 @@ builder.selenium2.rcPlayback.playNextStep = function() {
     builder.selenium2.rcPlayback.currentStepIndex < builder.selenium2.rcPlayback.script.steps.length)
   {
     builder.selenium2.rcPlayback.currentStep = builder.selenium2.rcPlayback.script.steps[builder.selenium2.rcPlayback.currentStepIndex];
-    builder.selenium2.rcPlayback.types[builder.selenium2.rcPlayback.currentStep.type.getName()](builder.selenium2.rcPlayback.currentStep);
+    jQuery('#' + builder.selenium2.rcPlayback.currentStep.id + '-content').css('background-color', '#ffffaa');
+   builder.selenium2.rcPlayback.types[builder.selenium2.rcPlayback.currentStep.type.getName()](builder.selenium2.rcPlayback.currentStep);
   } else {
     builder.selenium2.rcPlayback.shutdown();
   }
 };
 
-builder.selenium2.rcPlayback.shutdown = function() {
+builder.selenium2.rcPlayback.shutdown = function(fireAndForget) {
   // Finish session.
-  builder.selenium2.rcPlayback.post("", "", function(responseText) {
-    jQuery('#edit-rc-playing').hide();
-    jQuery('#edit-rc-stopping').hide();
-    if (builder.selenium2.rcPlayback.postRunCallback) {
-      builder.selenium2.rcPlayback.postRunCallback(builder.selenium2.rcPlayback.playResult);
+  builder.selenium2.rcPlayback.send("DELETE", "", "", function() {
+    if (!fireAndForget) {
+      jQuery('#edit-rc-playing').hide();
+      jQuery('#edit-rc-stopping').hide();
+      if (builder.selenium2.rcPlayback.postRunCallback) {
+        builder.selenium2.rcPlayback.postRunCallback(builder.selenium2.rcPlayback.playResult);
+      }
     }
-  }, "DELETE");
+  });
 };
 
 builder.selenium2.rcPlayback.stopTest = function() {
@@ -102,7 +115,35 @@ builder.selenium2.rcPlayback.stopTest = function() {
   jQuery('#edit-rc-stopping').show();
 };
 
-builder.selenium2.rcPlayback.post = function(path, msg, callback, http_method) {
+builder.selenium2.rcPlayback.hasError = function(response) {
+  return !!response.status; // So undefined and 0 are fine.
+};
+
+builder.selenium2.rcPlayback.handleError = function(response) {
+  var err = "Server Error";
+  if (response.value && response.value.message) {
+    err += ": " + response.value.message;
+  }
+  if (builder.selenium2.rcPlayback.currentStepIndex === -1) {
+    // If we can't connect to the server right at the start, just attach the error message to the
+    // first step.
+    builder.selenium2.rcPlayback.currentStepIndex = 0;
+  }
+  jQuery("#" + builder.selenium2.rcPlayback.script.steps[builder.selenium2.rcPlayback.currentStepIndex].id + '-content').css('background-color', '#ff3333');
+  jQuery("#" + builder.selenium2.rcPlayback.script.steps[builder.selenium2.rcPlayback.currentStepIndex].id + "-error").html(err).show();
+  builder.selenium2.rcPlayback.playResult.success = false;
+  builder.selenium2.rcPlayback.playResult.errormessage = err;
+  
+  builder.selenium2.rcPlayback.shutdown(/*fireAndForget*/true);
+  
+  jQuery('#edit-rc-playing').hide();
+  jQuery('#edit-rc-stopping').hide();
+  if (builder.selenium2.rcPlayback.postRunCallback) {
+    builder.selenium2.rcPlayback.postRunCallback(builder.selenium2.rcPlayback.playResult);
+  }
+};
+
+builder.selenium2.rcPlayback.send = function(http_method, path, msg, callback, errorCallback) {
   var url = null;
   if (builder.selenium2.rcPlayback.sessionID) {
     url = "http://" + builder.selenium2.rcPlayback.hostPort + "/wd/hub/session/" + builder.selenium2.rcPlayback.sessionID + path;
@@ -121,30 +162,22 @@ builder.selenium2.rcPlayback.post = function(path, msg, callback, http_method) {
     type: http_method || "POST",
     url: url,
     data: msg,
-    // Automatically chop off the null characters from the response.
-    success: function(t) { callback(builder.selenium2.rcPlayback.fixServerResponse(t)); },
-    error: builder.selenium2.rcPlayback.xhrfailed
+    success: function(t) {
+      if (callback) {
+        callback(builder.selenium2.rcPlayback.parseServerResponse(t));
+      } else {
+        builder.selenium2.rcPlayback.recordResult({'success': true});
+      }
+    },
+    error: function(xhr, textStatus, errorThrown) {
+      var response = builder.selenium2.rcPlayback.parseServerResponse(xhr.responseText);
+      if (errorCallback) {
+        errorCallback(response);
+      } else {
+        builder.selenium2.rcPlayback.handleError(response);
+      }
+    }
   });
-};
-
-builder.selenium2.rcPlayback.xhrfailed = function(xhr, textStatus, errorThrown) {
-  var err = "Server connection error: " + errorThrown;
-  if (builder.selenium2.rcPlayback.currentStepIndex === -1) {
-    // If we can't connect to the server right at the start, just attach the error message to the
-    // first step.
-    builder.selenium2.rcPlayback.currentStepIndex = 0;
-  }
-  jQuery("#" + builder.selenium2.rcPlayback.script.steps[builder.selenium2.rcPlayback.currentStepIndex].id + '-content').css('background-color', '#ff3333');
-  jQuery("#" + builder.selenium2.rcPlayback.script.steps[builder.selenium2.rcPlayback.currentStepIndex].id + "-error").html(err).show();
-  builder.selenium2.rcPlayback.playResult.success = false;
-  builder.selenium2.rcPlayback.playResult.errormessage = err;
-  jQuery('#edit-editing').show();
-  jQuery('#edit-rc-playing').hide();
-  jQuery('#edit-rc-stopping').hide();
-  
-  if (builder.selenium2.rcPlayback.postRunCallback) {
-    builder.selenium2.rcPlayback.postRunCallback(builder.selenium2.rcPlayback.playResult);
-  }
 };
 
 /** Performs ${variable} substitution for parameters. */
@@ -178,7 +211,7 @@ builder.selenium2.rcPlayback.param = function(pName) {
     }
   }
 
-  return builder.selenium2.rcPlayback.currentStep.type.getParamType(pName) == "locator" ? {"type": builder.selenium2.rcPlayback.currentStep[pName].getName(builder.selenium2), "value": output} : output;
+  return builder.selenium2.rcPlayback.currentStep.type.getParamType(pName) == "locator" ? {"using": builder.selenium2.rcPlayback.currentStep[pName].getName(builder.selenium2), "value": output} : output;
 };
 
 builder.selenium2.rcPlayback.print = function(text) {
@@ -206,31 +239,44 @@ builder.selenium2.rcPlayback.recordResult = function(result) {
 
 builder.selenium2.rcPlayback.types = {};
 
-builder.selenium2.rcPlayback.postOnly = function(path, msg) {
-  builder.selenium2.rcPlayback.post(path, msg, function() {
-    builder.selenium2.rcPlayback.recordResult({success: true});
-  });
+builder.selenium2.rcPlayback.findElement = function(locator, callback, errorCallback) {
+  builder.selenium2.rcPlayback.send("POST", "/element", JSON.stringify(locator),
+    function(response) {
+      if (builder.selenium2.rcPlayback.hasError(response)) {
+        if (errorCallback) {
+          errorCallback(response);
+        } else {
+          builder.selenium2.rcPlayback.handleError(response);
+        }
+      } else {
+        if (callback) {
+          callback(response.value.ELEMENT);
+        } else {
+          builder.selenium2.rcPlayback.recordResult({success: true});
+        }
+      }
+    }
+  );
 };
 
 builder.selenium2.rcPlayback.types.get = function(step) {
-  builder.selenium2.rcPlayback.postOnly("/url", JSON.stringify({'url': step.url}));
+  builder.selenium2.rcPlayback.send("POST", "/url", JSON.stringify({'url': step.url}));
 };
 
 builder.selenium2.rcPlayback.types.verifyTextPresent = function(step) {
-  builder.selenium2.rcPlayback.post("/element", JSON.stringify({'using': 'tag name', 'value': 'body'}),
-    function(responseText) {
-      var response = JSON.parse(responseText);
-      var id = response.value.ELEMENT;
-      builder.selenium2.rcPlayback.post("/element/" + id + "/text", "",
-        function(responseText) {
-          var response = JSON.parse(responseText);
+  builder.selenium2.rcPlayback.findElement({'using': 'tag name', 'value': 'body'},
+    /* success */
+    function(id) {
+      builder.selenium2.rcPlayback.send("GET", "/element/" + id + "/text", "",
+        /* success */
+        function(response) {
           if (response.value.indexOf(builder.selenium2.rcPlayback.param("text")) != -1) {
             builder.selenium2.rcPlayback.recordResult({success: true});
           } else {
             builder.selenium2.rcPlayback.recordResult({success: false, message: "Text not present."});
           }
-        },
-      "GET");
+        }
+      );
     }
   );
 };
