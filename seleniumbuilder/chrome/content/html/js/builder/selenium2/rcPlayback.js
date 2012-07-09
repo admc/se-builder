d0691f0e36044fa1b02cb5e7326afab0a9f69d64
David Stark <david.stark@zarkonnen.com>
2012-07-09 09:37:53 +0200

    More remote webdriver playback step types. Sample assert/verify/wait/store types.

--- seleniumbuilder/chrome/content/html/js/builder/selenium2/rcPlayback.js
+++ seleniumbuilder/chrome/content/html/js/builder/selenium2/rcPlayback.js
@@ -26,6 +26,14 @@ builder.selenium2.rcPlayback.script = null;
 builder.selenium2.rcPlayback.requestStop = false;
 builder.selenium2.rcPlayback.playResult = null;
 builder.selenium2.rcPlayback.vars = {};
+/** What interval to check waits for. */
+builder.selenium2.rcPlayback.waitIntervalAmount = 3000;
+/** How many wait cycles are run before waits time out. */
+builder.selenium2.rcPlayback.maxWaitCycles = 45000 / builder.selenium2.rcPlayback.waitIntervalAmount;
+/** How many wait cycles have been run. */
+builder.selenium2.rcPlayback.waitCycle = 0;
+/** The wait interval. */
+builder.selenium2.rcPlayback.waitInterval = null;
 
 builder.selenium2.rcPlayback.run = function(hostPort, browserstring, postRunCallback) {
   jQuery('#steps-top')[0].scrollIntoView(false);
@@ -80,7 +88,9 @@ builder.selenium2.rcPlayback.parseServerResponse = function(t) {
 builder.selenium2.rcPlayback.startJob = function(response) {
   builder.selenium2.rcPlayback.sessionID = response.sessionId;
   builder.selenium2.rcPlayback.playResult.success = true;
-  builder.selenium2.rcPlayback.playNextStep();
+  builder.selenium2.rcPlayback.send("POST", "/timeouts/implicit_wait", JSON.stringify({'ms':60000}), function(response) {
+    builder.selenium2.rcPlayback.playNextStep();
+  });
 };
 
 builder.selenium2.rcPlayback.playNextStep = function() {
@@ -96,15 +106,19 @@ builder.selenium2.rcPlayback.playNextStep = function() {
   }
 };
 
-builder.selenium2.rcPlayback.shutdown = function(fireAndForget) {
+builder.selenium2.rcPlayback.shutdown = function() {
   // Finish session.
   builder.selenium2.rcPlayback.send("DELETE", "", "", function() {
-    if (!fireAndForget) {
-      jQuery('#edit-rc-playing').hide();
-      jQuery('#edit-rc-stopping').hide();
-      if (builder.selenium2.rcPlayback.postRunCallback) {
-        builder.selenium2.rcPlayback.postRunCallback(builder.selenium2.rcPlayback.playResult);
-      }
+    jQuery('#edit-rc-playing').hide();
+    jQuery('#edit-rc-stopping').hide();
+    if (builder.selenium2.rcPlayback.postRunCallback) {
+      builder.selenium2.rcPlayback.postRunCallback(builder.selenium2.rcPlayback.playResult);
+    }
+  }, function() {
+    jQuery('#edit-rc-playing').hide();
+    jQuery('#edit-rc-stopping').hide();
+    if (builder.selenium2.rcPlayback.postRunCallback) {
+      builder.selenium2.rcPlayback.postRunCallback(builder.selenium2.rcPlayback.playResult);
     }
   });
 };
@@ -138,13 +152,7 @@ builder.selenium2.rcPlayback.recordError = function(err) {
   builder.selenium2.rcPlayback.playResult.success = false;
   builder.selenium2.rcPlayback.playResult.errormessage = err;
   
-  builder.selenium2.rcPlayback.shutdown(/*fireAndForget*/true);
-  
-  jQuery('#edit-rc-playing').hide();
-  jQuery('#edit-rc-stopping').hide();
-  if (builder.selenium2.rcPlayback.postRunCallback) {
-    builder.selenium2.rcPlayback.postRunCallback(builder.selenium2.rcPlayback.playResult);
-  }
+  builder.selenium2.rcPlayback.shutdown();
 };
 
 builder.selenium2.rcPlayback.send = function(http_method, path, msg, callback, errorCallback) {
@@ -241,8 +249,6 @@ builder.selenium2.rcPlayback.recordResult = function(result) {
   builder.selenium2.rcPlayback.playNextStep();
 };
 
-builder.selenium2.rcPlayback.types = {};
-
 builder.selenium2.rcPlayback.findElement = function(locator, callback, errorCallback) {
   builder.selenium2.rcPlayback.send("POST", "/element", JSON.stringify(locator),
     function(response) {
@@ -263,13 +269,163 @@ builder.selenium2.rcPlayback.findElement = function(locator, callback, errorCall
   );
 };
 
+builder.selenium2.rcPlayback.findElements = function(locator, callback, errorCallback) {
+  builder.selenium2.rcPlayback.send("POST", "/elements", JSON.stringify(locator),
+    function(response) {
+      if (builder.selenium2.rcPlayback.hasError(response)) {
+        if (errorCallback) {
+          errorCallback(response);
+        } else {
+          builder.selenium2.rcPlayback.handleError(response);
+        }
+      } else {
+        if (callback) {
+          var elids = [];
+          for (var i = 0; i < response.value.length; i++) {
+            elids.push(response.value[i].ELEMENT);
+          }
+          callback(elids);
+        } else {
+          builder.selenium2.rcPlayback.recordResult({success: true});
+        }
+      }
+    }
+  );
+};
+
+/** Repeatedly calls testFunction, allowing it to tell us if it was successful. */
+builder.selenium2.rcPlayback.wait = function(testFunction) {
+  builder.stepdisplay.setProgressBar(builder.selenium2.rcPlayback.currentStep.id, 0);
+  builder.selenium2.rcPlayback.waitCycle = 0;
+  builder.selenium2.rcPlayback.waitInterval = window.setInterval(function() {
+    testFunction(function(success) {
+      if (success != builder.selenium2.rcPlayback.currentStep.negated) {
+        window.clearInterval(builder.selenium2.rcPlayback.waitInterval);
+        builder.stepdisplay.hideProgressBar(builder.selenium2.rcPlayback.currentStep.id);
+        builder.selenium2.rcPlayback.recordResult({'success': success});
+        return;
+      }
+      if (builder.selenium2.rcPlayback.waitCycle++ >= builder.selenium2.rcPlayback.maxWaitCycles) {
+        window.clearInterval(builder.selenium2.rcPlayback.waitInterval);
+        builder.stepdisplay.hideProgressBar(builder.selenium2.rcPlayback.currentStep.id);
+        builder.selenium2.rcPlayback.recordError("Wait timed out.");
+        return;
+      }
+      if (builder.selenium2.rcPlayback.stopRequest) {
+        window.clearInterval(builder.selenium2.rcPlayback.waitInterval);
+        builder.stepdisplay.hideProgressBar(builder.selenium2.rcPlayback.currentStep.id);
+        builder.selenium2.rcPlayback.shutdown();
+        return;
+      }
+      builder.stepdisplay.setProgressBar(builder.selenium2.rcPlayback.currentStep.id, builder.selenium2.rcPlayback.waitCycle * 100 / builder.selenium2.rcPlayback.maxWaitCycles);
+    });
+  }, builder.selenium2.rcPlayback.waitIntervalAmount);
+};
+
+builder.selenium2.rcPlayback.types = {};
+
+builder.selenium2.rcPlayback.types.print = function(step) {
+  builder.selenium2.rcPlayback.print(builder.selenium2.rcPlayback.param("text"));
+  builder.selenium2.rcPlayback.recordResult({success: true});
+};
+
+builder.selenium2.rcPlayback.types.store = function(step) {
+  builder.selenium2.rcPlayback.vars[builder.selenium2.rcPlayback.param("variable")] = builder.selenium2.rcPlayback.param("text");
+  builder.selenium2.rcPlayback.recordResult({success: true});
+};
+
 builder.selenium2.rcPlayback.types.get = function(step) {
   builder.selenium2.rcPlayback.send("POST", "/url", JSON.stringify({'url': step.url}));
+};
+
+builder.selenium2.rcPlayback.types.goBack = function(step) {
+  builder.selenium2.rcPlayback.send("POST", "/back", "");
+};
+
+builder.selenium2.rcPlayback.types.goForward = function(step) {
+  builder.selenium2.rcPlayback.send("POST", "/forward", "");
+};
+
+builder.selenium2.rcPlayback.types.refresh = function(step) {
+  builder.selenium2.rcPlayback.send("POST", "/refresh", "");
 };
 
 builder.selenium2.rcPlayback.types.clickElement = function(step) {
   builder.selenium2.rcPlayback.findElement(builder.selenium2.rcPlayback.param("locator"), function(id) {
     builder.selenium2.rcPlayback.send("POST", "/element/" + id + "/click", "");
+  });
+};
+
+builder.selenium2.rcPlayback.types.doubleClickElement = function(step) {
+  builder.selenium2.rcPlayback.findElement(builder.selenium2.rcPlayback.param("locator"), function(id) {
+    builder.selenium2.rcPlayback.send("POST", "/element/" + id + "/click", "", function(response) {
+      builder.selenium2.rcPlayback.send("POST", "/element/" + id + "/click", "");
+    });
+  });
+};
+
+builder.selenium2.rcPlayback.types.submitElement = function(step) {
+  builder.selenium2.rcPlayback.findElement(builder.selenium2.rcPlayback.param("locator"), function(id) {
+    builder.selenium2.rcPlayback.send("POST", "/element/" + id + "/submit", "");
+  });
+};
+
+builder.selenium2.rcPlayback.types.setElementText = function(step) {
+  builder.selenium2.rcPlayback.findElement(builder.selenium2.rcPlayback.param("locator"), function(id) {
+    builder.selenium2.rcPlayback.send("POST", "/element/" + id + "/click", "", function(response) {
+      builder.selenium2.rcPlayback.send("POST", "/element/" + id + "/clear", "", function(response) {
+        builder.selenium2.rcPlayback.send("POST", "/element/" + id + "/value", JSON.stringify({
+          'value': [builder.selenium2.rcPlayback.param("text")]
+        }));
+      });
+    });
+  });
+};
+
+builder.selenium2.rcPlayback.types.sendKeysToElement = function(step) {
+  builder.selenium2.rcPlayback.findElement(builder.selenium2.rcPlayback.param("locator"), function(id) {
+    builder.selenium2.rcPlayback.send("POST", "/element/" + id + "/click", "", function(response) {
+      builder.selenium2.rcPlayback.send("POST", "/element/" + id + "/value", JSON.stringify({
+        'value': [builder.selenium2.rcPlayback.param("text")]
+      }));
+    });
+  });
+};
+
+builder.selenium2.rcPlayback.types.setElementSelected = function(step) {
+  builder.selenium2.rcPlayback.findElement(builder.selenium2.rcPlayback.param("locator"), function(id) {
+    builder.selenium2.rcPlayback.send("GET", "/element/" + id + "/selected", "", function(response) {
+      if (response.value) {
+        builder.selenium2.rcPlayback.recordResult({success: true});
+      } else {
+        builder.selenium2.rcPlayback.send("POST", "/element/" + id + "/click", "");
+      }
+    });
+  });
+};
+
+builder.selenium2.rcPlayback.types.setElementNotSelected = function(step) {
+  builder.selenium2.rcPlayback.findElement(builder.selenium2.rcPlayback.param("locator"), function(id) {
+    builder.selenium2.rcPlayback.send("GET", "/element/" + id + "/selected", "", function(response) {
+      if (!response.value) {
+        builder.selenium2.rcPlayback.recordResult({success: true});
+      } else {
+        builder.selenium2.rcPlayback.send("POST", "/element/" + id + "/click", "");
+      }
+    });
+  });
+};
+
+builder.selenium2.rcPlayback.types.clearElements = function(step) {
+  builder.selenium2.rcPlayback.findElements(builder.selenium2.rcPlayback.param("locator"), function(ids) {
+    for (var i = 0; i < ids.length; i++) {
+      builder.selenium2.rcPlayback.send("GET", "/element/" + ids[i] + "/selected", "", function(response) {
+        if (response.value) {
+          builder.selenium2.rcPlayback.send("POST", "/element/" + ids[i] + "/click", "", function(r){});
+        }
+      });
+      builder.selenium2.rcPlayback.recordResult({success: true}); // qqDPS Reporting success before completion!
+    }
   });
 };
 
@@ -293,6 +449,25 @@ builder.selenium2.rcPlayback.types.assertTextPresent = function(step) {
       } else {
         builder.selenium2.rcPlayback.recordError("Text not present.");
       }
+    });
+  });
+};
+
+builder.selenium2.rcPlayback.types.waitForTextPresent = function(step) {
+  builder.selenium2.rcPlayback.wait(function(callback) {
+    builder.selenium2.rcPlayback.findElement({'using': 'tag name', 'value': 'body'}, function(id) {
+      builder.selenium2.rcPlayback.send("GET", "/element/" + id + "/text", "", function(response) {
+        callback(response.value.indexOf(builder.selenium2.rcPlayback.param("text")) != -1);
+      }, /*error*/ function() { callback(false); });
+    }, /*error*/ function() { callback(false); });
+  });
+};
+
+builder.selenium2.rcPlayback.types.storeTextPresent = function(step) {
+  builder.selenium2.rcPlayback.findElement({'using': 'tag name', 'value': 'body'}, function(id) {
+    builder.selenium2.rcPlayback.send("GET", "/element/" + id + "/text", "", function(response) {
+      builder.selenium2.rcPlayback.vars[builder.selenium2.rcPlayback.param("variable")] = response.value.indexOf(builder.selenium2.rcPlayback.param("text")) != -1;
+      builder.selenium2.rcPlayback.recordResult({success: true});
     });
   });
 };
